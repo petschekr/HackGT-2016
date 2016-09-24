027fbed15c66481f560568891d746a37174fe663
Ryan Petschek <petschekr@gmail.com>
2016-09-24 13:53:14 -0400

    Add framework for scriping login page

---
+++ js/login.js
@@ -0,0 +1,17 @@
+"use strict";
+
+scrypt_module_factory(function (scrypt) {
+    // - Confirm login with server
+    // - Download encrypted user information
+    // - Derive key for decryption
+
+    var loginButton = document.getElementById("login");
+    loginButton.onclick(function() {
+        
+    });
+    var joinButton = document.getElementById("join");
+    joinButton.onclick(function() {
+
+    });
+
+});
